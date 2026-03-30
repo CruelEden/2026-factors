bb23c0ae370785c4f4b7805285f88b51dac7bc50
CruelEden <mayesp50477@masseyhigh.school.nz>
2026-03-31 10:51:45 +1300

    need help with exit

---
+++ D_02_int_check.py
@@ -0,0 +1,27 @@
+# Ask user for integer and loop they
+# enter a number that is more than one but lower then 200
+def int_check(question, low, high):
+
+    error = f"please enter number that is more then or equal to {low} or higher then {high}\n"
+    while True:
+
+        try:
+            # ask the user for a number
+            response = int(input(question))
+
+            # check that the number is more than one
+            if integer or 1 <= response <= 200:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine Goes Here
+for item in range(0,20):
+    integer = int_check("Integer: ", 1, 200)
+    print(integer)
+
+    print()
