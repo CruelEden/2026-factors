fa4f6947826a3ca96712c33b9fd18bcf7237d277
CruelEden <mayesp50477@masseyhigh.school.nz>
2026-04-02 10:23:18 +1300

    this one not work

--- D_02_int_check.py
+++ D_02_int_check.py
@@ -5,12 +5,15 @@ def int_check(question, low, high):
     error = f"please enter number that is more then or equal to {low} or higher then {high}\n"
     while True:
 
-        try:
-            # ask the user for a number
-            response = int(input(question))
+        # ask the user for a number or exit
+        response = int(input(question))
+        if response == "xxx":
+            return response
 
-            # check that the number is more than one
-            if exit == "xxx" or 1 <= response <= 200:
+        try:
+
+            # check that the number is more than 0 and less than 200
+            if {low} <= response <= {high}:
                 return response
             else:
                 print(error)
@@ -19,12 +22,8 @@ def int_check(question, low, high):
             print(error)
 
 
+
 # Main Routine Goes Here
 for item in range(0,20):
     integer = int_check("Integer: ", 1, 200)
     print(integer)
-
-    print()
-
-    if exit == "xxx":
-        break
