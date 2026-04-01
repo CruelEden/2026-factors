b26fcb1c6cfa23bfbef40501de63604efaf403d5
CruelEden <mayesp50477@masseyhigh.school.nz>
2026-04-02 10:41:59 +1300

    fixed int_check

--- D_02_int_check.py
+++ D_02_int_check.py
@@ -1,29 +1,29 @@
 # Ask user for integer and loop they
 # enter a number that is more than one but lower then 200
-def int_check(question, low, high):
+def num_check(question):
 
-    error = f"please enter number that is more then or equal to {low} or higher then {high}\n"
+    error = f"please enter number that is between 1 and 200 inclusive\n"
     while True:
 
         # ask the user for a number or exit
-        response = int(input(question))
+        response = input(question).lower()
         if response == "xxx":
             return response
 
         try:
+            # ask for number
+            response = int(response)
 
-            # check that the number is more than 0 and less than 200
-            if {low} <= response <= {high}:
+            # check that the number is more than 0 and less than or equal to 200
+            if 1 <= response <= 200:
                 return response
             else:
                 print(error)
-
         except ValueError:
             print(error)
 
 
-
 # Main Routine Goes Here
 for item in range(0,20):
-    integer = int_check("Integer: ", 1, 200)
+    integer = num_check("Integer: ")
     print(integer)
